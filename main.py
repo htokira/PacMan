053b24d92dc9352a6d1c6ae9a77566730b25343e
Drehel Ivanna <drehelivanka@knu.ua>
2026-02-12 22:35:05 +0200

    update-pacman-in-main

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from settings import *
 from menu import Menu
 from map import Map
 from levels import LEVEL1
+from pacman import Pacman
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pac-Man")
 activeLevel = LEVEL1
 game_map = Map(screen, activeLevel, WIDTH, HEIGHT)
-
+player = Pacman(100, 100)
 def run_game():
     while True:
         screen.fill(BLACK)
         pygame.display.set_caption("Game Running...")
         game_map.draw_map()
+        player.update(game_map)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +25,7 @@ def run_game():
                 sys.exit()
         
         pygame.display.update()
+        player.draw(screen)
 
 def main():
     menu = Menu(screen)
